b63cbc27cc517d1ca3ad853d31c335758a6d9b8b
Parth-Dab <78088321+Parth-Dab@users.noreply.github.com>
2021-04-19 16:39:49 +0530

    commited by parth
    i have done file already exist error solved.. and add validation in delete..
    and also added variable in error.h file...

--- OurDB_Database/Parth_module/create_database/create_database.h
+++ OurDB_Database/Parth_module/create_database/create_database.h
@@ -37,25 +37,32 @@ string createDatabase(string databaseName)
        cn = databaseName.back(); // stores last character of string
       if(cm >= 97 && cm <= 122)   // check whether the first character is between "a to z" or not
        {
-          if((cn >= 32 && cn <= 47)  ||(cn >= 58 && cn <= 92) || (cn >= 123 && cn <= 126)) { // check for special character
-              return errorSpecialchaDatabase[0]; // defined in Errors/error_variable.h
-          }else{
-                  databaseName = strPath[0] + databaseName; //strpath[0]="path" defined in  variables/query_variables.h
-               if(!(filesystem::exists(databaseName.c_str()))) // checks for database that has already been created
-               {
+          if((cn >= 32 && cn <= 47)  ||(cn >= 58 && cn <= 92) || (cn >= 123 && cn <= 126)) // check for special character
+          {
 
-                   if (mkdir(databaseName.c_str()) ==-1) { // cretae directory, mkdir function defined in direct.h header file
-                       return errorCreatingDatabase[0]; // defined in Errors/error_variable.h
-                   } else {
-                       databaseName = databaseName +
-                                      "/deafault.Ourdb"; //it takes file path from above and add the path to file
-                       //  FileTabel(databaseName); //creats file called default.Ourdb from above defined function
-                       return SuccessCreatingDatabaseMsg[0]; // Success_Messages/Success_Msg.h
+              return errorSpecialchaDatabase[0]; // defined in Errors/error_variable.h
+
+          }else{
+                   databaseName = strPath[0] + databaseName; //strpath[0]="path" defined in  variables/query_variables.h
+                   if(!(filesystem::exists(databaseName.c_str()))) // checks for database that has already been created
+                   {
+
+                       if(mkdir(databaseName.c_str()) ==-1)  // cretae directory, mkdir function defined in direct.h header file
+                       {
+
+                           return errorCreatingDatabase[0]; // defined in Errors/error_variable.h
+
+                       } else{
+                          // databaseName = databaseName +
+                                          "/deafault.Ourdb"; //it takes file path from above and add the path to file
+                           //  FileTabel(databaseName); //creats file called default.Ourdb from above defined function
+
+                           return SuccessCreatingDatabaseMsg[0]; // Success_Messages/Success_Msg.h
+                       }
+
+                   }else{
+                        return errorDatatbaseExists[0];// defined in error.h
                    }
-               }else
-               {
-                    return errorDatatbaseExists[0];// defined in error.h
-               }
           }
        }
        else if(!(cm >= 97 && cm <= 122)) // check if character is not between "a to z"...
