f35b301de373d20fed6c02f9a4dfbfb0e073b309
zeelsheladiya <46935793+zeelsheladiya@users.noreply.github.com>
2021-07-01 17:58:41 +0530

    where with additional funct added

--- OurDB_Database/select_module/zeel_select_module/SelectFuncForAllWithWhereWithAdditionalFunc.h
+++ OurDB_Database/select_module/zeel_select_module/SelectFuncForAllWithWhereWithAdditionalFunc.h
@@ -7,7 +7,79 @@
 
 string SelectFuncForAllWithWhereWithAdditionalFunc(string tablename, string additionalFunc , vector<string> afterat)
 {
-    return " ";
+    //string for where condition
+    string whereConditionString;
+
+    // convert vector into string
+    for(int i = 3 ; i < afterat.size(); i++ )
+    {
+        whereConditionString += afterat[i];
+    }
+
+    // string of index where condition mathed
+    string indexies = globalFuncForWhereClouse(whereConditionString,tablename,2);
+
+    // regex for onliy number
+    regex regexForOnlyInt("[0-9]{1,}");
+
+
+    if(regex_match(indexies,regexForOnlyInt))
+    {
+        //main logic
+
+        string tablepath = databaseSavePath +"/"+ tablename +".Ourdb";
+
+        ifstream in(tablepath);
+
+        ourdb coldata;
+
+        in >> coldata;
+
+        vector<string> ind;
+
+        for(auto zeel : indexies)
+        {
+            ind.insert(ind.end(),to_string(zeel-48));
+        }
+
+        vector<string> colname;     //vector string for col names
+        vector<vector<string>> data;    //vector of vector string for table data
+
+        for (auto& x : coldata["records"]["col_names"].items())
+            colname.insert(colname.end(), to_string(x.value()));    //col name inserted to vector colname
+
+        for(int i=0; i < ind.size();i++)
+        {
+            vector<string> jval;    //temporary vector to store value to add into vector of vector 'data'
+            for(auto &j : coldata["table_data"][stoi(ind[i])].items())
+            {
+                jval.push_back(decryption(j.value()));      //value pushed to vector of vector string 'data'
+            }
+            data.push_back(jval);
+        }
+
+        if(additionalFunc == "<")
+        {
+            ascending_sort(data);
+        }
+        else if(additionalFunc == ">")
+        {
+            descending_sort(data);
+        }
+
+        return SelectQueryStructureCreater(colname,data);         //global func for select query
+    }
+    else if (indexies == "")
+    {
+        // error no data found
+        return ErrNoDataFoundInTable[0];
+    }
+    else
+    {
+        // it will give you an error
+        return indexies;
+    }
+
 }
 
 #endif //OURDB_DATABASE_SELECTFUNCFORALLWITHWHEREWITHADDITIONALFUNC_H
--- OurDB_Database/select_module/zeel_select_module/SelectFuncForColsWithWhereWithAdditionalFunc.h
+++ OurDB_Database/select_module/zeel_select_module/SelectFuncForColsWithWhereWithAdditionalFunc.h
@@ -7,7 +7,79 @@
 
 string SelectFuncForColsWithWhereWithAdditionalFunc(string tablename, string additionalFunc , vector<string> beforeat , vector<string> afterat)
 {
-    return " ";
+    //string for where condition
+    string whereConditionString;
+
+    // convert vector into string
+    for(int i = 3 ; i < afterat.size(); i++ )
+    {
+        whereConditionString += afterat[i];
+    }
+
+    // string of index where condition mathed
+    string indexies = globalFuncForWhereClouse(whereConditionString,tablename,2);
+
+    // regex for onliy number
+    regex regexForOnlyInt("[0-9]{0,}");
+
+
+    if(regex_match(indexies,regexForOnlyInt))
+    {
+
+        string tablepath = databaseSavePath +"/"+ tablename +".Ourdb";
+
+        ourdb our1;
+        fstream fs(tablepath);
+        vector <vector<string>> data;
+        string ml;
+        fs >> our1;
+
+        vector<string> ind;
+
+        for(auto zeel : indexies)
+        {
+            ind.insert(ind.end(),to_string(zeel-48));
+        }
+
+        for(int k=0 ;k<ind.size();k++)      //loop for data that satisfies after where condition
+        {
+            vector<string> newvec;
+            for (int i = 0; i < our1["records"]["col_names"].size(); i++)   //loop for checking col names with col indexes
+            {
+                for (int j = 0; j < beforeat.size(); j++)   //loop for checking cols that are specified by user before '@'
+                {
+                    if (our1["records"]["col_names"][i] == beforeat[j])     //checks user specified col with col in table
+                    {
+                        string l = to_string(our1["records"]["col_index"][i]); // takes the value from column index ..
+                        newvec.insert(newvec.end(), decryption(our1["table_data"][stoi(ind[k])][l]));
+                    }
+                }
+
+            }
+            data.push_back(newvec);     //inserting vector 'newvec' into vector of vector 'data'
+        }
+
+        if(additionalFunc == "<")
+        {
+            ascending_sort(data);
+        }
+        else if(additionalFunc == ">")
+        {
+            descending_sort(data);
+        }
+
+        return SelectQueryStructureCreater(beforeat,data);
+    }
+    else if (indexies == "")
+    {
+        // error no data found
+        return ErrNoDataFoundInTable[0];
+    }
+    else
+    {
+        // it will give you an error
+        return indexies;
+    }
 }
 
 #endif //OURDB_DATABASE_SELECTFUNCFORCOLSWITHWHEREWITHADDITIONALFUNC_H
