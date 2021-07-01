79c4240e1c93b53b02d331054f0ece0ea8d2cfbb
Parth-Dab <78088321+Parth-Dab@users.noreply.github.com>
2021-07-01 17:30:39 +0530

    update selectFunForColWithAdditionalFunc

--- OurDB_Database/select_module/parth_select_module/SelectFuncForColWithAdditionalFunc.h
+++ OurDB_Database/select_module/parth_select_module/SelectFuncForColWithAdditionalFunc.h
@@ -7,7 +7,40 @@
 
 string SelectFuncForColswithAdditionalFunc(string tablename , string AdditionalFunc ,vector <string> beforeat)
 {
-    return " ";
+    ourdb our1; // object of json
+    fstream fs(tablename);  // file pointer points to tablePath
+    vector <vector<string>> data; //2d vector array...
+    fs >> our1; // data of tablePath to json object..
+    if(our1["table_data"].size() > 0) {   // check whether there is data in table or not
+
+        for(int k=0 ;k<our1["table_data"].size();k++) {  // iterate through every row in [table_data]
+            vector<string> newvec;  // create new string vector for storing single row data in it...
+            for (int i = 0; i < our1["records"]["col_names"].size(); i++) { //
+
+                for (int j = 0; j < beforeat.size(); j++) { // iterate through beforeat(column name) vector
+
+                    if (our1["records"]["col_names"][i] == beforeat[j]) { // it checks data are same in beforeat and json object
+                        string l = to_string(our1["records"]["col_index"][i]); // takes the value from column index ..
+                        newvec.insert(newvec.end(), decryption(our1["table_data"][k][l])); //insert decrypted data in newvec
+                    }
+                }
+
+            }
+            data.push_back(newvec); // it will push beck data newvec into data vector..
+        }
+
+        if(AdditionalFunc == "↑")
+        {
+            sort(data.begin(),data.end());
+        }else if(AdditionalFunc == "↑")
+        {
+            sort(data.end(),data.begin());
+        }
+        return SelectQueryStructureCreater(beforeat,data); // it will return string.. function defined in global fun for select
+    }else{
+        return "error";
+    }
+
 }
 
 #endif //OURDB_DATABASE_SELECTFUNCFORCOLWITHADDITIONALFUNC_H
